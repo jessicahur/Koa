b327679863ad1221507f4ed47e47743020f8d074
Jessica Tran Hur <jesshur824@gmail.com>
2016-01-18 19:01:46 -0800

    working get and post

--- app.js
+++ app.js
@@ -1,18 +1,112 @@
 var app = require('koa')();
 var router = require('koa-router')();
+var koaBody   = require('koa-body')();
+var fs = require('fs');
+var dataStorage = './dataStorage/';
 
 
 // REST
 router.get('/notes', function*(next) {
 
+  yield new Promise((resolve, reject) => {
+
+    fs.readdir(dataStorage, (err, filesList) => {
+      if (err) {
+        this.body = ('Cannot get notes list. Try again later.');
+        return console.log('Cannot read directory of dataStorage: ',err.message);
+      }
+      filesList = filesList.filter(function(file){
+        return file.charAt(0) !== '.';
+      });
+      this.body = 'Files in data storage: '+filesList.join(', ');
+      resolve();
+    });
+
+  });
 });
 
-router.get('/notes/:id', function*(next) {
+router
+  .param('file', function*(id, next) {
+    this.file = id;
+    yield next;
+  })
+  .get('/notes/:file', function*(next) {
+    var requestedFile = this.file+'.json';
 
-});
+    yield new Promise((resolve, reject) => {
 
-router.post('/notes', function*(next) {
+      fs.readdir(dataStorage,  (err, filesList) => {
+      if (filesList.indexOf(requestedFile) !== -1) {
+        var path = dataStorage+requestedFile;
 
+        fs.readFile(path, (err, data) => {
+          if (err) {
+            this.body = 'Cannot get requested file.';
+            resolve();//NOT SURE IF NEEDED HERE
+            return console.log('Cannot read requested file:',err.message);
+          }
+          var content = JSON.parse(data.toString());
+          this.body = content;
+          resolve();
+        });
+      }
+      else{
+        this.body = 'Cannot load file content';
+        resolve();
+      }
+    });
+    });
+  });
+
+//POST
+router.post('/notes', koaBody, function*(next) {
+
+  var dataBody = JSON.stringify(this.request.body);
+
+  yield new Promise( (resolve, reject) => {
+
+    fs.readdir(dataStorage, (err, filesList) => {
+    if (err) {
+      resolve();
+      return console.log('Cannot read directory of dataStorage: ',err.message);
+    }
+      filesList = filesList.filter(function(file){
+          return file.charAt(0) !== '.';
+      });
+
+      //if there is no json in data storage
+      if (filesList.length === 0) { //annoying .DS_Store
+        var fileName = '1.json';
+
+        fs.writeFile(dataStorage+fileName, dataBody, (err) => {
+          if (err) {
+            this.body = 'Data failed to save. Try again later.';
+            resolve();
+            return console.log(err.message);
+          }
+
+          this.body = 'SUCCESS! Data written to server storage: '+ dataBody+' in '+fileName;
+          resolve();
+        });
+      }
+
+      //if there are json files in storage
+      else {
+        fileName = ++filesList.length;
+        fileName = fileName.toString()+'.json';
+
+        fs.writeFile(dataStorage+fileName, dataBody, (err) => {
+          if (err) {
+            this.body = 'Data failed to save. Try again later.';
+            resolve();
+            return console.log(err.message);
+          }
+          this.body = 'SUCCESS! Data written to server storage: '+ dataBody+' in '+fileName;
+          resolve();
+        });
+      }
+    });
+  });
 });
 
 router.put('/notes/:id', function*(next) {
